8c32317274105d196039f93f1d437c46ef026436
Michelle Poterek <poterekm@gmail.com>
2017-08-11 16:14:03 -0700

    add hard coded data to act as model in routes/index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+  var questions = [{
+    id: 1,
+    username: "user",
+    category: "category",
+    title: "title",
+    body: "body"
+  }, {
+    id: 2,
+    username: "xoxCodingCutie",
+    category: "Typing",
+    title: "How do I type?",
+    body: "When I move my fingers nothing happens"
+  }];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
