946e0bd0ad8f5a897fde73bfaa7888793ac1c59e
Michelle Poterek <poterekm@gmail.com>
2017-08-12 11:09:35 -0700

    answers display appropriately on each individual question

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+     var params = {
+       name: this.get('name'),
+       level: this.get('level'),
+       answer: this.get('answer'),
+       question: this.get('question')
+     };
+     this.set('addNewAnswer', false);
+     this.sendAction('saveAnswer', params);
+   }
+  }
 });
